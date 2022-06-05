5bc674697aae75cd662b58675d3940065c434dbf
Harsh_616 <harsh36diwaker@gmail.com>
2022-06-06 04:06:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mist/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mist
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val addbutton: Button = findViewById(R.id.addbutton)
+        addbutton.setOnClickListener{
+            val intent= Intent(this,Activity2::class.java)
+            startActivity(intent)
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
